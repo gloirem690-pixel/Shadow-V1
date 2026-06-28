af826fa8b358a7e43d47b0c46ab1f0953c992764
gloirem690-pixel <gloirem690@gmail.com>
2026-06-28 09:58:27 +0100

    Initialize handlers module with imports

---
+++ handlers/__init__.py
@@ -0,0 +1,8 @@
+from .start import start, help_command, status
+from .ask import ask_command, vision_command, ask_on, ask_off, handle_message
+from .model import change_model, set_prompt
+from .image import generate_image
+from .tts import text_to_speech
+from .translate import translate_command
+from .stats import stats_command
+from .memory import clear_memory
